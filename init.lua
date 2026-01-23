15cfffeaf0edb2a775021844d7f8a884289e4bc8
Ivan Karabadzhak <jakeroid@icloud.com>
2026-01-23 10:01:01 +0200

    removed unnecessary log print

--- init.lua
+++ init.lua
@@ -9,13 +9,11 @@ require("config.settings")
 -- This file should be placed in lua/custom/settings.lua and is ignored by git
 local custom_settings_path = vim.fn.stdpath('config') .. '/lua/custom/settings.lua'
 if vim.fn.filereadable(custom_settings_path) == 1 then
-  -- Use pcall for safety, in case the user's file has errors
-  local ok, err = pcall(require, 'custom.settings')
-  if not ok then
-    vim.notify("Error loading custom/settings.lua:\n" .. tostring(err), vim.log.levels.ERROR)
-  else
-     vim.notify("Loaded custom settings from lua/custom/settings.lua", vim.log.levels.INFO)
-  end
+    -- Use pcall for safety, in case the user's file has errors
+    local ok, err = pcall(require, 'custom.settings')
+    if not ok then
+        vim.notify("Error loading custom/settings.lua:\n" .. tostring(err), vim.log.levels.ERROR)
+    end
 end
 
 -- Load Plugins Manager (lazy.nvim) and setup plugins
