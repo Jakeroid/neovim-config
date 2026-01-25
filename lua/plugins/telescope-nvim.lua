d2b18fbfaf1f334ee27c7063db5beb66b0af41b2
Ivan Karabadzhak <jakeroid@icloud.com>
2026-01-25 23:00:36 +0200

    improved telescope config by adding there live grep args plugin

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -8,12 +8,18 @@ return {
                 "nvim-telescope/telescope-fzf-native.nvim",  -- FZF support
                 build = "make",  -- Compiles the native FZF sorter
             },
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim" ,
+                -- This will not install any breaking changes.
+                -- For major updates, this must be adjusted manually.
+                version = "^1.0.0",
+            },
             "debugloop/telescope-undo.nvim",
         },
         cmd = "Telescope",
         keys = {
             { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+            { "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Live grep" },
             { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
             { "<leader>fr", "<cmd>Telescope resume<cr>", desc = "Resume last search" },
             { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
@@ -24,15 +30,18 @@ return {
             { "<leader>fu", "<cmd>Telescope undo<cr>", desc = "Undo Plugin" },
         },
         config = function()
-            require("telescope").setup {
+            local telescope = require("telescope")
+            local lga_actions = require("telescope-live-grep-args.actions")
+            telescope.setup {
                 defaults = {
                     file_ignore_patterns = { "node_modules", ".git/", "dist/", "venv/" },
                     mappings = {
-                        i = {
-                            ["<C-j>"] = "move_selection_next",
-                            ["<C-k>"] = "move_selection_previous",
-                            ["<C-q>"] = "send_selected_to_qflist",
-                        },
+                        -- at this point I decided to disabled them in "i"
+                        -- i = {
+                        --     ["<C-j>"] = "move_selection_next",
+                        --     ["<C-k>"] = "move_selection_previous",
+                        --     ["<C-q>"] = "send_selected_to_qflist",
+                        -- },
                     },
                 },
                 extensions = {
@@ -44,10 +53,27 @@ return {
                     },
                     undo = {
                     },
+                    live_grep_args = {
+                        auto_quoting = true, -- enable/disable auto-quoting
+                        -- define mappings, e.g.
+                        mappings = { -- extend mappings
+                            i = {
+                                ["<C-k>"] = lga_actions.quote_prompt(),
+                                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                                -- freeze the current list and start a fuzzy search in the frozen list
+                                -- ["<C-y>"] = lga_actions.to_fuzzy_refine,
+                            },
+                        },
+                        -- ... also accepts theme settings, for example:
+                        -- theme = "dropdown", -- use dropdown theme
+                        -- theme = { }, -- use own theme spec
+                        -- layout_config = { mirror=true }, -- mirror preview pane
+                    }
                 },
             }
-            require("telescope").load_extension("fzf")  
-            require("telescope").load_extension("undo")
+            telescope.load_extension("fzf")  
+            telescope.load_extension("undo")
+            telescope.load_extension("live_grep_args")
         end,
     }
 }
